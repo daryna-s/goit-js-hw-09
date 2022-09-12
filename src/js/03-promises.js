a4c80f53f74349080b95e513aab9586ad5be18cd
daryna-s <lypenn@gmail.com>
2022-09-13 00:20:45 +0300

    done task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,17 +8,18 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', e => { 
   e.preventDefault();
 
+
   let { delay, step, amount } = createSubmit();
 
   for (let position = 1; position <= amount; position += 1){
     createPromise(position, delay)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-        Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
     delay += step;
   }
@@ -47,3 +48,4 @@ function createSubmit() {
     amount: Number(amount.value),
   };
 }
+
