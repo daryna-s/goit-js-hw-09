3540637c784a860d0265451c8bb4ff9d0e6041bb
daryna-s <lypenn@gmail.com>
2022-09-13 19:02:48 +0300

    update task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,24 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 let timerId = null;
+stopBtn.disabled = true;
 
-
-  startBtn.addEventListener('click', () => {
+startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
     timerId = setInterval(() => {
       document.body.setAttribute(
         'style',
         'background-color: ' + getRandomHexColor()
-      );
-        console.log(`Цвет меняется`);
-            startBtn.disabled = true;
-            stopBtn.disabled = false;
+      )            
     }, 1000);
   });
 
-stopBtn.addEventListener('click', () => {
-    clearInterval(timerId);
-    console.log(`Цвет НЕ меняется`);
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
+stopBtn.addEventListener('click', () => {    
+        clearInterval(timerId);
+        startBtn.disabled = false;
+        stopBtn.disabled = true;
+    
 });
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
